afe57d59525b13b26b3f4babe71deabe5c9af95a
jiangxin hu <jiangxin_hu@aliyun.com>
2016-12-15 10:45:40 +0800

    Add three py files for test
    Just for test

---
+++ 02 - print-test.py
@@ -0,0 +1,13 @@
+#import sys
+
+print(eval("4+9"),'','d')
+
+print("Hello World!", '', '\n', None, True)
+
+exec("help(eval)")
+
+name = "Angel"
+age = 12
+school = "PingHe"
+
+print ("My name is: %s" % name )
---
+++ 03 - dict-test.py
@@ -0,0 +1,38 @@
+# 测试dict函数是否copy 
+addr = {'Hellen':'1234','Angel':'4567'}
+addr2 = dict(addr)
+addr['Angel'] = '5678'
+print (addr)
+print (addr2)
+
+# 测试字典的使用
+people = {
+    'Alice':{
+        'phone': '2341',
+        'addr' : 'Foo drive 23'
+        },
+    'Beth': {
+        'phone': '9102',
+        'addr' : 'Bar street 42'
+        },
+    'Cecil': {
+        'phone': '3178',
+        'addr' : 'Baz avenue 90'
+        }
+    }
+
+labels = {'phone': 'phone number',
+          'addr': 'address'
+          }
+
+name = input('Name;')
+request = input("Phone number (p) or address (a)?")
+                
+if request == 'p' : key = "phone"
+if request == 'a' : key = "addr"
+print (key)
+
+if name in people: print ("%s's %s is %s." % \
+                         (name, labels[key], people[name][key]) )
+
+print ("All the information for Alice is " + "%(Alice)s" % people )
